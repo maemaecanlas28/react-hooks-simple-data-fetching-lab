5e6b50b0a5f7ade1c2297a9c28d19a7f12f4389f
Flordeliza Mae Canlas <maemaeperlas@gmail.com>
2022-07-26 20:22:01 -0700

    done with the assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,30 @@
 // create your App component here
+import React, {useState, useEffect} from "react";
+
+function App () {
+
+    const [data, setData] = useState("")
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    function handleData (data) {
+        setData(data.message)
+        setIsLoaded(true)
+    }
+
+    useEffect (() => {
+        fetch ("https://dog.ceo/api/breeds/image/random")
+        .then(data => data.json())
+        .then(handleData)
+    }, [])
+
+    return (
+        <>
+            {isLoaded ? (
+                <img src={data} alt="A Random Dog" />
+            ):
+            (<p> Loading... </p>)}
+        </>
+    )
+}
+
+export default App
